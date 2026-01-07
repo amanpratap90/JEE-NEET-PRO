d199708bf6e6a8af2d44865fae907227ac7a3563
amanpratap90 <amanpratap9076@gmail.com>
2026-01-07 15:54:48 +0530

    Update App.jsx

--- frontend/JEE-NEET/src/App.jsx
+++ frontend/JEE-NEET/src/App.jsx
@@ -5,8 +5,8 @@ import Footer from "./layout/Footer"
 import ScrollToTop from "./Components/ScrollToTop";
 import { Suspense, lazy } from "react";
 import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-import AdminRoute from "./components/AdminRoute";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import AdminRoute from "./Components/AdminRoute";
 
 import TestSeries from "./pages/TestSeries";
 
